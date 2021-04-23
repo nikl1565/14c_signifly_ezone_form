0f14fe47e46a330261816709f22079d7ca781042
Niklas Vejlby Schjoldager <nikl1565@stud.kea.dk>
2021-04-24 00:47:00 +0200

    Added fonts, colors etc

--- main.js
+++ main.js
@@ -1,6 +1,1 @@
 import "./sass/main.scss";
-
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
